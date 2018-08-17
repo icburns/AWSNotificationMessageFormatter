a108ad92efe1099ccf2bfebe428271dc6b6828d8
2D\ianb <ianb@slalom.com>
2018-08-16 19:17:25 -0700

    Parse CodeCommit message bodies for menaingful subjects

--- CodeCommitMessageParser/CodeCommitMessageParser.Designer.cs
+++ CodeCommitMessageParser/CodeCommitMessageParser.Designer.cs
@@ -15,7 +15,7 @@ namespace CodeCommitMessageParser {
     /// 
     [Microsoft.VisualStudio.Tools.Applications.Runtime.StartupObjectAttribute(0)]
     [global::System.Security.Permissions.PermissionSetAttribute(global::System.Security.Permissions.SecurityAction.Demand, Name="FullTrust")]
-    public sealed partial class ThisAddIn : Microsoft.Office.Tools.Outlook.OutlookAddInBase {
+    public sealed partial class CodeCommitMessageParser : Microsoft.Office.Tools.Outlook.OutlookAddInBase {
         
         internal Microsoft.Office.Tools.CustomTaskPaneCollection CustomTaskPanes;
         
@@ -28,7 +28,7 @@ namespace CodeCommitMessageParser {
         /// 
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Never)]
-        public ThisAddIn(global::Microsoft.Office.Tools.Outlook.Factory factory, global::System.IServiceProvider serviceProvider) : 
+        public CodeCommitMessageParser(global::Microsoft.Office.Tools.Outlook.Factory factory, global::System.IServiceProvider serviceProvider) : 
                 base(factory, serviceProvider, "AddIn", "ThisAddIn") {
             Globals.Factory = factory;
         }
@@ -40,7 +40,7 @@ namespace CodeCommitMessageParser {
         protected override void Initialize() {
             base.Initialize();
             this.Application = this.GetHostItem<Microsoft.Office.Interop.Outlook.Application>(typeof(Microsoft.Office.Interop.Outlook.Application), "Application");
-            Globals.ThisAddIn = this;
+            Globals.CodeCommitMessageParser = this;
             global::System.Windows.Forms.Application.EnableVisualStyles();
             this.InitializeCachedData();
             this.InitializeControls();
@@ -174,21 +174,21 @@ namespace CodeCommitMessageParser {
         private Globals() {
         }
         
-        private static ThisAddIn _ThisAddIn;
+        private static CodeCommitMessageParser _CodeCommitMessageParser;
         
         private static global::Microsoft.Office.Tools.Outlook.Factory _factory;
         
         private static ThisRibbonCollection _ThisRibbonCollection;
-
+        
         private static ThisFormRegionCollection _ThisFormRegionCollection;
         
-        internal static ThisAddIn ThisAddIn {
+        internal static CodeCommitMessageParser CodeCommitMessageParser {
             get {
-                return _ThisAddIn;
+                return _CodeCommitMessageParser;
             }
             set {
-                if ((_ThisAddIn == null)) {
-                    _ThisAddIn = value;
+                if ((_CodeCommitMessageParser == null)) {
+                    _CodeCommitMessageParser = value;
                 }
                 else {
                     throw new System.NotSupportedException();
@@ -222,7 +222,7 @@ namespace CodeCommitMessageParser {
         internal static ThisFormRegionCollection FormRegions {
             get {
                 if ((_ThisFormRegionCollection == null)) {
-                    _ThisFormRegionCollection = new ThisFormRegionCollection(Globals.ThisAddIn.GetFormRegions());
+                    _ThisFormRegionCollection = new ThisFormRegionCollection(Globals.CodeCommitMessageParser.GetFormRegions());
                 }
                 return _ThisFormRegionCollection;
             }
@@ -263,13 +263,13 @@ namespace CodeCommitMessageParser {
         
         internal WindowFormRegionCollection this[Microsoft.Office.Interop.Outlook.Explorer explorer] {
             get {
-                return ((WindowFormRegionCollection)(Globals.ThisAddIn.GetFormRegions(explorer, typeof(WindowFormRegionCollection))));
+                return ((WindowFormRegionCollection)(Globals.CodeCommitMessageParser.GetFormRegions(explorer, typeof(WindowFormRegionCollection))));
             }
         }
         
         internal WindowFormRegionCollection this[Microsoft.Office.Interop.Outlook.Inspector inspector] {
             get {
-                return ((WindowFormRegionCollection)(Globals.ThisAddIn.GetFormRegions(inspector, typeof(WindowFormRegionCollection))));
+                return ((WindowFormRegionCollection)(Globals.CodeCommitMessageParser.GetFormRegions(inspector, typeof(WindowFormRegionCollection))));
             }
         }
     }
---
+++ CodeCommitMessageParser/CodeCommitMessageParser.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using Microsoft.Office.Interop.Outlook;
+
+namespace CodeCommitMessageParser
+{
+    public partial class CodeCommitMessageParser
+    {
+		const string CODE_COMMIT_MESSAGE_SUBJECT = "AWS Notification Message";
+		const string CODE_COMMIT_EMAIL = "no-reply@sns.amazonaws.com";
+		const string CODE_COMMIT_BODY_PREFIX = "\"A pull request event occurred in the following AWS CodeCommit repository: ";
+		const string COMMENT_IDENTIFIER = "made a comment or replied to a comment ";
+		const string CREATED_IDENTIFIER = "made the following PullRequest ";
+		const string MERGED_IDENTIFIER = "The status is merged. ";
+		const string PULL_REQUEST_NUMBER_IDENTIFIER = "the following PullRequest";
+
+		bool fullParseRequired = true;
+
+		Explorer currentExplorer;
+
+		private void CodeCommitMessageParser_Startup(object sender, EventArgs e)
+        {
+			currentExplorer = Application.ActiveExplorer();
+			currentExplorer.SelectionChange += new ExplorerEvents_10_SelectionChangeEventHandler(CurrentExplorer_Event);
+		}
+
+		private void CodeCommitMessageParser_Shutdown(object sender, EventArgs e)
+		{
+			// Note: Outlook no longer raises this event. If you have code that 
+			//    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+		}
+
+		private void CurrentExplorer_Event()
+		{
+			if (!fullParseRequired)
+			{
+				return;
+			}
+
+			Folder selectedFolder =
+				this.Application.ActiveExplorer().CurrentFolder as Folder;
+
+			if (selectedFolder.Name != "Inbox")
+			{
+				return;
+			}
+
+			foreach (var item in selectedFolder.Items) {
+
+				if (item is MailItem)
+				{
+					MailItem mailItem = item as MailItem;
+					if (//mailItem.Subject == CODE_COMMIT_MESSAGE_SUBJECT &&
+						//mailItem.SenderEmailAddress == CODE_COMMIT_EMAIL &&
+						mailItem.Body != null &&
+						mailItem.Body.Contains("A pull request event occurred in the following AWS CodeCommit repository:"))//mailItem.Body.StartsWith(CODE_COMMIT_BODY_PREFIX))
+					{
+						mailItem.Subject = GetNewSubject(mailItem.Body);
+						mailItem.Save();
+					}
+				}
+			}
+
+			fullParseRequired = false;
+		}
+
+		private string GetNewSubject(string body)
+		{
+			int startUserIndex = body.IndexOf("arn:aws:") + 8;
+			string[] userTokens = body.Substring(startUserIndex).Split(' ')[0].Split('/');
+			string user = userTokens[userTokens.Length - 1];
+
+			string action = "";
+			if (body.Contains(COMMENT_IDENTIFIER))
+			{
+				action = "commented on";
+			}
+			else if (body.Contains(CREATED_IDENTIFIER))
+			{
+				action = "created";
+			}
+			else if (body.Contains(MERGED_IDENTIFIER))
+			{
+				action = "merged";
+			}
+			else
+			{
+				action = "updated";
+			}
+
+			int pullRequestNumberIndex = body.IndexOf(PULL_REQUEST_NUMBER_IDENTIFIER + ": ") != -1 ?
+											body.IndexOf(PULL_REQUEST_NUMBER_IDENTIFIER + ": ") :
+											body.IndexOf(PULL_REQUEST_NUMBER_IDENTIFIER + " ");
+			string pullRequestNumber = Regex.Replace(body.Substring(pullRequestNumberIndex + PULL_REQUEST_NUMBER_IDENTIFIER.Length).Split(' ')[1], "[^0-9]", "");
+			string repository = body.Substring(CODE_COMMIT_BODY_PREFIX.Length).Split(' ')[0];
+			string reference = $"pull request {pullRequestNumber} in {repository}";
+
+			return $"{user} {action} {reference}";
+		}
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+        {
+            Startup += new EventHandler(CodeCommitMessageParser_Startup);
+            Shutdown += new EventHandler(CodeCommitMessageParser_Shutdown);
+        }
+        
+    }
+}
